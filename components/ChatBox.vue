4c34e0cff6cbb0dcb78603b1295157efc038aba8
Eli Finer <eli.finer@gmail.com>
2023-05-11 22:34:01 -0700

    integrate openai

--- components/ChatBox.vue
+++ components/ChatBox.vue
@@ -1,17 +1,17 @@
 <template>
 <div class="text-white bg-gray-500 flex flex-col h-screen mx-auto sm:py-5">
   <div class="mb-6 flex-grow overflow-y-auto flex flex-col-reverse mb-2 px-2 sm:px-0">
-    <div v-for="(message, index) in messages.slice().reverse()" :key="index" 
+    <div v-for="(message, index) in messages.slice().reverse()" :key="index"
          class="p-4 rounded-none w-full"
-         :class="{'bg-gray-600': message.sender === 'user', 'bg-gray-500': message.sender === 'bot'}">
+         :class="{'bg-gray-600': message.role === 'user', 'bg-gray-500': message.role === 'bot'}">
       <div class="max-w-2xl mx-auto">
         <div class="flex">
           <div class="w-8">
-            <div v-if="message.sender === 'user'" class="mr-2"><UserIcon class="h-6 w-6 text-blue-200" /></div>
+            <div v-if="message.role === 'user'" class="mr-2"><UserIcon class="h-6 w-6 text-blue-200" /></div>
             <div v-else class="mr-2"><PrinterIcon class="h-6 w-6 text-blue-200" /></div>
           </div>
           <div class="flex-grow">
-            {{ message.text }}
+            {{ message.content }}
           </div>
         </div>
       </div>
@@ -38,8 +38,8 @@ export default {
   methods: {
     async sendMessage() {
       const message = {
-        text: this.newMessage,
-        sender: 'user',
+        content: this.newMessage,
+        role: 'user',
       }
       this.messages.push(message)
       this.newMessage = ''
@@ -47,8 +47,8 @@ export default {
       const response = await $fetch('/api/chat', { method: 'post', body: { messages: this.messages } })
 
       this.messages.push({
-        text: response.message,
-        sender: 'bot',
+        content: response.message,
+        role: 'assistant',
       })
     },
   },
--- server/api/chat.post.js
+++ server/api/chat.post.js
@@ -1,6 +1,29 @@
+import { Configuration, OpenAIApi } from 'openai';
+let openai;
+
+const instructions = ``;
+
+if (process.env.OPENAI_API_KEY) {
+    const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+    openai = new OpenAIApi(configuration);
+}
+
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
+
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        temperature: 0.7,
+        max_tokens: 2048,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        top_p: 1,
+        messages: body.messages,
+    }, { timeout: 60000 });
+
+    const response_text = response.data.choices[0].message.content.trim();
+
     return {
-      message: 'You said ' + body.messages[body.messages.length - 1].text
+      message: response_text,
     }
 })
